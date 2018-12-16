ed160e9d7863a9d8508fb126be392a6c0b6df183
yasin <luoyasin@gmail.com>
2018-12-16 11:05:33 +0800

    first commit

---
+++ local.py
@@ -0,0 +1,10 @@
+VERIFY_TOKEN = ""
+GRAPH_URL = "https://graph.facebook.com/v2.6"
+ACCESS_TOKEN = ""
+
+# imgur key
+client_id = ''
+client_secret = ''
+album_id = ''
+access_token = ''
+refresh_token = ''
